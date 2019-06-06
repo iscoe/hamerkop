07134fc9c4984d1d3ad61d1fabef2b68f27667ac
Cash Costello <cash.costello@jhuapl.edu>
2019-06-06 14:36:10 -0400

    added a memory-baked kb

--- hamerkop/kb.py
+++ hamerkop/kb.py
@@ -1,5 +1,12 @@
 from abc import ABC, abstractmethod
+import collections
+import csv
+import logging
+import os
 
+from .core import Entity
+
+logger = logging.getLogger(__name__)
 
 """LoReHLT Knowledge Base columns"""
 KBColumns = [
@@ -53,6 +60,10 @@ KBColumns = [
 ]
 
 
+class KBException(Exception):
+    """An error occurred when interacting with the KB."""
+
+
 class KB(ABC):
     """
     Knowledge base interface
@@ -87,7 +98,7 @@ class KB(ABC):
         pass
 
 
-class NameIndex(ABC):
+class NameSearch(ABC):
     """
     Find candidates based on a name string
     """
@@ -101,3 +112,68 @@ class NameIndex(ABC):
         :return: list of Entity objects
         """
         pass
+
+
+class MemoryKB(KB):
+    """
+    KB backed by a python dictionary for smaller kbs
+    """
+    def __init__(self, entities_filename, alt_names_filename):
+        """
+        :param entities_filename: filename for the entities file
+        :param alt_names_filename: filename for the alternate names file
+        """
+        for filename in [entities_filename, alt_names_filename]:
+            if not os.path.exists(filename):
+                raise KBException("{} does not exist".format(filename))
+
+        self.entities = self._read_entities(entities_filename)
+        self._add_alt_names(self.entities, alt_names_filename)
+
+    def size(self):
+        return len(self.entities)
+
+    def get_entity(self, entity_id):
+        return self.entities.get(entity_id)
+
+    def get_entities(self, entity_ids):
+        return [self.entities[x] for x in entity_ids if x in self.entities]
+
+    def __iter__(self):
+        for entity in self.entities.values():
+            yield entity
+
+    def _generate_entities(self, entity_rows, name_rows):
+        entities = collections.OrderedDict()
+        for row in entity_rows:
+            if row['entityid'] not in entities:
+                entity = Entity(row['entityid'], row['entity_type'], row['name'], row['origin'],
+                            None, str(row['latitude']), str(row['longitude']),
+                            row['country_code'], row['population'], row['external_link'])
+                entities[entity.id] = entity
+        for row in name_rows:
+            entities[row['entityid']].names.add(row['alternatename'])
+        return list(entities.values())
+
+    def _read_entities(self, filename):
+        entities = {}
+        with open(filename, 'r') as fp:
+            reader = csv.reader(fp, delimiter='\t', quoting=csv.QUOTE_NONE)
+            next(reader)
+            for row in reader:
+                entity = self._generate_entity(row)
+                entities[entity.id] = entity
+        return entities
+
+    def _add_alt_names(self, entities, filename):
+        with open(filename, 'r') as fp:
+            reader = csv.reader(fp, delimiter='\t', quoting=csv.QUOTE_NONE)
+            next(reader)
+            for row in reader:
+                entity_id = row[0]
+                alt_name = row[1]
+                entities[entity_id].names.add(alt_name)
+
+    @staticmethod
+    def _generate_entity(row):
+        return Entity(row[2], row[1], row[3], row[0], None, row[5], row[6], row[12], row[21])
---
+++ tests/test_kb.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+from hamerkop.kb import *
+
+
+def get_filename(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+
+class MemoryKBTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        entities_filename = get_filename('data/kb/small_kb_entities.tab')
+        alt_names_filename = get_filename('data/kb/small_kb_alternate_names.tab')
+        cls.kb = MemoryKB(entities_filename, alt_names_filename)
+
+    def test_getting_person(self):
+        entity = self.kb.get_entity('10')
+        self.assertEqual('10', entity.id)
+        self.assertIn('JDawg', entity.names)
+
+    def test_getting_person_not_in_kb(self):
+        entity = self.kb.get_entity('13')
+        self.assertIsNone(entity)
+
+    def test_getting_multiple_entities(self):
+        entities = self.kb.get_entities(['1', '5', '11'])
+        self.assertEqual(2, len(entities))
+
+    def test_iterator(self):
+        entity_ids = [entity.id for entity in self.kb]
+        self.assertEqual(4, len(entity_ids))
+        self.assertIn('10', entity_ids)
