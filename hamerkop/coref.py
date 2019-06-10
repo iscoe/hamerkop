5dd457442f906553431ff1a83b61828f56a69e39
Cash Costello <cash.costello@jhuapl.edu>
2019-06-10 10:54:28 -0400

    adds second sub-stage to coref

--- hamerkop/coref.py
+++ hamerkop/coref.py
@@ -1,14 +1,18 @@
 import abc
+import logging
 
 from .core import EntityType, MentionChain
 from .utilities import CaseInsensitiveDict
 
+logger = logging.getLogger(__name__)
+
 
 class CoRef(abc.ABC):
     """
-    Indoc Coreference to create mention chains
-    """
+    Within Document Coreference
 
+    Turns mentions into mention chains
+    """
     @abc.abstractmethod
     def coref(self, document):
         """
@@ -16,6 +20,40 @@ class CoRef(abc.ABC):
         :param document: Document
         """
         pass
+
+
+class CoRefUpdate(abc.ABC):
+    """
+    Update the current mention chains (split or merge)
+    """
+    @abc.abstractmethod
+    def update(self, document):
+        """
+        Process the current mention chains to merge or split some
+        :param document: Document
+        """
+        pass
+
+    def merge(self, chains, *to_merge):
+        logger.debug('{} merging {}'.format(self.__class__.__name__, to_merge))
+        new_chain = MentionChain(sum([chain.mentions for chain in to_merge], []))
+        chains = [x for x in chains if x not in to_merge]
+        chains.append(new_chain)
+        return chains
+
+
+class TwoStageCoRef(CoRef):
+    """
+    Create mention chains with a CoRef component and then update with one or more CoRefUpdate components
+    """
+    def __init__(self, coref, updaters):
+        self._coref = coref
+        self._updaters = updaters
+
+    def coref(self, document):
+        self._coref.coref(document)
+        for updater in self._updaters:
+            updater.update(document)
 
 
 class UnchainedCoRef(CoRef):
@@ -42,3 +80,47 @@ class ExactMatchCoRef(CoRef):
         for entity_type in chain_data:
             chains = [MentionChain(chain) for chain in chain_data[entity_type].values()]
             document.mention_chains.extend(chains)
+
+
+class CoRefAcronymUpdate(CoRefUpdate):
+    """
+    Finds best match for names that look like acronyms
+
+    This only works for scripts that support case.
+    This does not handle words that are dropped like 'of'.
+    """
+    def __init__(self, min_length):
+        """
+        :param min_length: Minimum length in characters
+        """
+        self.min_length = min_length
+
+    def update(self, document):
+        acronym_chains = [chain for chain in document.mention_chains if self.is_acronym(chain.name)]
+        if acronym_chains:
+            other_chains = [(chain, self.create_acronym(chain.name)) for chain in document.mention_chains
+                            if chain not in acronym_chains]
+            for acronym_chain in acronym_chains:
+                matches = []
+                for chain, acronym in other_chains:
+                    if acronym_chain.type != chain.type:
+                        continue
+                    if acronym_chain.name == acronym:
+                        matches.append(chain)
+                if matches:
+                    matches = self.rank(acronym_chain, matches)
+                    document.mention_chains = self.merge(document.mention_chains, acronym_chain, matches[0])
+
+    @staticmethod
+    def rank(acronym_chain, matches):
+        # TODO for now default to first instance
+        return matches
+
+    def is_acronym(self, string):
+        if len(string) < self.min_length:
+            return False
+        return string.upper() == string
+
+    @staticmethod
+    def create_acronym(string):
+        return ''.join(word[0].upper() for word in string.split())
--- tests/test_coref.py
+++ tests/test_coref.py
@@ -1,7 +1,27 @@
 import unittest
-from hamerkop.core import Document, EntityType, Mention
+from hamerkop.core import Document, EntityType, Mention, MentionChain
 from hamerkop.coref import *
 from hamerkop.lang import Lang
+
+
+class CoRefUpdateTest(unittest.TestCase):
+    class DummyMerger(CoRefUpdate):
+        def update(self, document):
+            pass
+
+    def test_merge(self):
+        chains = [
+            MentionChain([
+                Mention('1', '_NW_1', (), (), EntityType.PER, 'Men1'),
+                Mention('4', '_NW_1', (), (), EntityType.PER, 'Men4')
+            ]),
+            MentionChain([Mention('2', '_NW_1', (), (), EntityType.PER, 'Men2')]),
+            MentionChain([Mention('3', '_NW_1', (), (), EntityType.PER, 'Men3')]),
+        ]
+        updater = CoRefUpdateTest.DummyMerger()
+        chains = updater.merge(chains, chains[0], chains[2])
+        self.assertEqual(2, len(chains))
+        self.assertEqual(3, len(chains[-1].mentions))
 
 
 class UnchainedCoRefTest(unittest.TestCase):
@@ -31,3 +51,34 @@ class ExactMatchCoRefTest(unittest.TestCase):
 
         self.assertEqual(3, len(doc.mention_chains))
         self.assertEqual(sorted([1, 1, 3]), sorted(list(map(len, doc.mention_chains))))
+
+
+class CoRefAcronymUpdateTest(unittest.TestCase):
+    def test_create_acronym(self):
+        self.assertEqual('SC', CoRefAcronymUpdate.create_acronym('south carolina'))
+        self.assertEqual('M', CoRefAcronymUpdate.create_acronym('Maryland'))
+
+    def test_is_acronym(self):
+        updater = CoRefAcronymUpdate(3)
+        self.assertTrue(updater.is_acronym('USA'))
+        self.assertTrue(updater.is_acronym('UNCHR'))
+        self.assertFalse(updater.is_acronym('LoReHLT'))
+        self.assertFalse(updater.is_acronym('US'))
+
+    def test_is_acronym2(self):
+        doc = Document([
+            Mention('South Carolina', '_DF_doc34', (141, 149), (22, 23), EntityType.GPE, 'Men1'),
+            Mention('SC', '_DF_doc34', (146, 154), (24, 25), EntityType.GPE, 'Men2'),
+            Mention('south carolina', '_DF_doc34', (173, 181), (36, 37), EntityType.PER, 'Men3'),
+            Mention('ed Smith', '_DF_doc34', (186, 194), (51, 52), EntityType.PER, 'Men4'),
+            Mention('Ed Smith', '_DF_doc34', (237, 245), (71, 72), EntityType.ORG, 'Men5'),
+        ], [], Lang.EN)
+        doc.mention_chains = [
+            MentionChain([doc.mentions[0]]),
+            MentionChain([doc.mentions[1]]),
+            MentionChain([doc.mentions[2]]),
+            MentionChain([doc.mentions[3], doc.mentions[4]]),
+        ]
+        updater = CoRefAcronymUpdate(2)
+        updater.update(doc)
+        self.assertEqual(3, len(doc.mention_chains))
