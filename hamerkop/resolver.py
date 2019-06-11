2e6b017d2aa4e0619a0849fd22e29f1cd12bc378
Cash Costello <cash.costello@jhuapl.edu>
2019-06-11 17:31:49 -0400

    adds scoring for resolvers

--- hamerkop/resolver.py
+++ hamerkop/resolver.py
@@ -1,5 +1,7 @@
 import abc
 
+from .io import LinkType
+
 
 class Resolver(abc.ABC):
     """Resolve links between mention chains and kb entities"""
@@ -11,3 +13,66 @@ class Resolver(abc.ABC):
         :param document: Document with mention chains and candidate sets
         """
         pass
+
+
+class ResolverReport:
+    """Report about Resolver Performance"""
+    def __init__(self):
+        # recall denominator
+        self.num_mentions_with_correct_candidate = 0
+        # numerator
+        self.num_mentions_correct_entity = 0
+        # precision denominator
+        self.num_mentions_with_links = 0
+
+    @property
+    def precision(self):
+        if self.num_mentions_with_links:
+            return self.num_mentions_correct_entity / self.num_mentions_with_links
+        else:
+            return 0
+
+    @property
+    def recall(self):
+        if self.num_mentions_with_correct_candidate:
+            return self.num_mentions_correct_entity / self.num_mentions_with_correct_candidate
+        else:
+            return 0
+
+    @property
+    def f1(self):
+        if self.precision + self.recall == 0:
+            return 0
+        return 2 * self.precision * self.recall / (self.precision + self.recall)
+
+
+class ResolverScorer:
+    """Incremental Resolver Scoring"""
+    def __init__(self, gt):
+        """
+        :param gt: Ground truth from OutputReader ({doc -> {offsets -> link}})
+        """
+        self.gt = gt
+        self.report = ResolverReport()
+
+    def update(self, document):
+        """
+        Incrementally update the scores
+        :param document: Document with mention chains that have candidates and entity
+        """
+        if document.docid in self.gt:
+            doc_gt = self.gt[document.docid]
+            for chain in document.mention_chains:
+                candidates = [x.id for x in chain.candidates]
+                for mention in chain.mentions:
+                    # precision denominator
+                    if chain.entity:
+                        self.report.num_mentions_with_links += 1
+                    # numerator and recall denominator
+                    if mention.offsets in doc_gt:
+                        link = doc_gt[mention.offsets]
+                        if link.link_type == LinkType.LINK:
+                            if set(link.links).intersection(set(candidates)):
+                                self.report.num_mentions_with_correct_candidate += 1
+                                if chain.entity and chain.entity.id in link.links:
+                                    self.report.num_mentions_correct_entity += 1
